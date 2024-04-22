cf2b230962dc043d4e6dc322212fa0c2639f592b
Jonas Holbech <jh@jonasholbech.dk>
2024-04-22 22:49:54 +0200

    nu er cors vidst nok væk

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 export async function GET(request) {
   const searchParams = {
-    url: "https://tv2.dk",
+    url: "https://bolig.dk",
   };
   const params = new URLSearchParams(searchParams);
   const response = await fetch(
     `https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`
   );
-  return NextResponse.json(response.json());
+  return NextResponse.json(await response.json());
 }
